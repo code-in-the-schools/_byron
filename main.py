7e9f70a0626fe3fc5997ece0bfb5e011f62181ac
byronbrown <68286443+byronbrown@users.noreply.github.com>
2020-07-20 16:43:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+vowels=["a","e","i","o","u"]
+numvowles-0
+numconsonants-0
+for i in newinput:
+  if i in vowles:#check vowel list
+ print (i+"is a vowel")
+  numVowles=numVowles +1
+print(i+ "is a consonant")
+  numConstant - numconsonants +1
+  print("# of vowels: " + int (numvowels)
+  print ("# of consonants: " + int (numconsonants))
